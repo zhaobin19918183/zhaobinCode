2da4c0378ccef3330f18a56cd2df443dddcfff5a
Zhao.bin <bin.zhao@network.innocellence.com>
2016-04-07 09:41:28 +0800

    no message

---
+++ miniship/miniship/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miniship
+//
+//  Created by Zhao.bin on 16/4/7.
+//  Copyright © 2016年 Zhao.bin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
