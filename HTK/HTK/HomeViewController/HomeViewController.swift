fdf2fbf230c27cfdd8f6463300d045330f75611c
赵斌 <zhixiang@zhaobin.local>
2016-05-17 20:44:12 +0800

    no message

--- HTK/HTK/HomeViewController/HomeViewController.swift
+++ HTK/HTK/HomeViewController/HomeViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // Do any additional setup after loading the view.
     }
@@ -20,7 +22,7 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+        
 
     /*
     // MARK: - Navigation
