585a3e120785351043f5d942988d3270363ae6f2
Zhao.bin <bin.zhao@network.innocellence.com>
2016-07-08 09:05:52 +0800

    no message

---
+++ Mac_os/Mac_osTests/Mac_osTests.swift
@@ -0,0 +1,36 @@
+//
+//  Mac_osTests.swift
+//  Mac_osTests
+//
+//  Created by Zhao.bin on 16/7/7.
+//  Copyright © 2016年 Zhao.bin. All rights reserved.
+//
+
+import XCTest
+@testable import Mac_os
+
+class Mac_osTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
